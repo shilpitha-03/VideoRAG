f3cfe6fcf8d8807f69f7f1820899a5746aa1ec28
shilpitha-03 <shilpitha.t03@gmail.com>
2026-06-20 11:55:10 -0400

    kg_baseline, chunk.py and llm.py pushed

---
+++ kg_baseline/chunk.py
@@ -0,0 +1,28 @@
+import json
+
+def get_chunks(json_path, windows_per_chunk):
+    data = json.load(open(json_path))
+    windows = data["windows"]
+    chunks = []
+
+    for w in range(0, len(windows), windows_per_chunk):
+        group = windows[w:w+windows_per_chunk]
+        
+        idxs = []
+        descs = []
+        for i in group:
+            idxs.append(w["window_idx"])
+            descs.append(f"[w{w["window_idx"]}]{w["description"]}")
+
+        chunk = {}
+
+        chunk["t_start"] = group[0]["t_start"]
+        chunk["t_end"] = group[-1]["t_end"]
+        chunk["window_idx"] = idxs
+        chunk["description"] = "\n".join(descs)
+        chunk["chunk_id"] = w // windows_per_chunk
+
+        chunks.append(chunk)         
+
+
+    return chunks
---
+++ kg_baseline/llm.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+import os
+
+#setup openai client on deepseek as it uses the openai protocol to run inference
+client = OpenAI(
+    api_key = os.environ["DEEPSEEK_API_KEY"], #the key is in the environment.
+    base_url = "https://api.deepseek.com"
+)
+
+def complete(prompt, system=None):
+    messages =[]
+    
+    if system:
+        messages.append({"role":"system", "content":system})
+    messages.append({"role":"user","content":prompt})
+    resp = client.chat.completions.create(model = "deepseek-chat", message=messages)
+    return resp.choices[0].message.content
+print(complete("Say hi!"))
